f48c98ccb14db37455c019ed0791433eab725fc4
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-06-22 23:38:10 +0200

    Initial Commit

---
+++ Star Wars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Star Wars
+//
+//  Created by Michele Manniello on 22/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Star Wars/Star_WarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_WarsApp.swift
+//  Star Wars
+//
+//  Created by Michele Manniello on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Star_WarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
